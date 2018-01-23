bacb1a65ed2540f703ca677a9ec1868207a2275c
Kate McHugh-Westfall <kate.mchugh94@gmail.com>
2018-01-23 21:53:41 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,12 +4,27 @@ require 'pry'
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
-# loction: project.css("span.location-name").text
+# loction: project.css("ul.project-meta span.location-name").text
+# percent funded: project.css("ul.project-stats li.first.funded strong").text
+
 
 def create_project_hash
   # This just opens a file and reads it into a variable
     html = File.read('fixtures/kickstarter.html')
     kickstarter = Nokogiri::HTML(html)
+    projects = {}
+
+    kickstarter.css("li.project.grid_4").each do |project|
+      title = project.css("h2.bbcard_name strong a").text
+      projects[title.to_sym] = {
+        :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+        :description => project.css("p.bbcard_blurb").text,
+        :location => project.css("ul.project-meta span.location-name").text,
+        :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+  end
+
+projects
+
 end
-binding.pry
-create_project_hash
+
